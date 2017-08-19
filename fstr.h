e72b706ad4ba2c025ca7581da5d2f94568c9dc68
Recolic Keghart <admin@recolic.net>
2017-08-19 23:30:12 +0800

    fstr header safe.

--- fstr.h
+++ fstr.h
@@ -6,45 +6,49 @@
 #include <cstdlib>
 #include <stdexcept>
 namespace rlib {
-    char *fstr(const char *fmt, ...) // must free after usage.
+    class fstr
     {
-        int n;
-        int size = 100;     /* Guess we need no more than 100 bytes */
-        char *p, *np;
-        va_list ap;
+    public:
+        char *operator()(const char *fmt, ...)
+        {
+            int n;
+            int size = 100;     /* Guess we need no more than 100 bytes */
+            char *p, *np;
+            va_list ap;
 
-        if ((p = (char *)malloc(size)) == NULL)
-            throw std::runtime_error("malloc returns null.");
+            if ((p = (char *)malloc(size)) == NULL)
+                throw std::runtime_error("malloc returns null.");
 
-        while (1) {
+            while (1) {
 
-            /* Try to print in the allocated space */
+                /* Try to print in the allocated space */
 
-            va_start(ap, fmt);
-            n = vsnprintf(p, size, fmt, ap);
-            va_end(ap);
+                va_start(ap, fmt);
+                n = vsnprintf(p, size, fmt, ap);
+                va_end(ap);
 
-            /* Check error code */
+                /* Check error code */
 
-            if (n < 0)
-                throw std::runtime_error("vsnprintf returns " + std::to_string(n));
+                if (n < 0)
+                    throw std::runtime_error("vsnprintf returns " + std::to_string(n));
 
-            /* If that worked, return the string */
+                /* If that worked, return the string */
 
-            if (n < size)
-                return p;
+                if (n < size)
+                    return p;
 
-            /* Else try again with more space */
+                /* Else try again with more space */
 
-            size = n + 1;       /* Precisely what is needed */
+                size = n + 1;       /* Precisely what is needed */
 
-            if ((np = (char *)realloc (p, size)) == NULL) {
-                free(p);
-                throw std::runtime_error("make_message realloc failed.");
-            } else {
-                p = np;
+                if ((np = (char *)realloc (p, size)) == NULL) {
+                    free(p);
+                    throw std::runtime_error("make_message realloc failed.");
+                } else {
+                    p = np;
+                }
             }
-        }
-    } //must free after usage.
+        } //must free after usage.
+    };
 }
 #endif //SRC_FSTR_H
