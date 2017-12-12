8ee86327f9cf704740db179f1ee68b977f631615
Recolic Keghart <root@recolic.net>
2017-12-12 20:13:01 +0800

    ...

--- stdio.hpp
+++ stdio.hpp
@@ -5,21 +5,22 @@
 
 namespace rlib {
     class io {
+    public:
     	template<typename PrintFinalT>
-    	void print(PrintFinalT reqArg)
+    	static void print(PrintFinalT reqArg)
     	{
     		::std::cout << reqArg;
     		return;
     	}
     	template<typename Required, typename... Optional>
-    	void print(Required reqArgs, Optional... optiArgs)
+    	static void print(Required reqArgs, Optional... optiArgs)
     	{
     		::std::cout << reqArgs << ' ';
     		print(optiArgs ...);
     		return;
     	}
     	template<typename... Optional>
-    	void println(Optional... optiArgs)
+    	static void println(Optional... optiArgs)
     	{
     		print(optiArgs ...);
     		::std::cout << ::std::endl;
@@ -27,35 +28,35 @@ namespace rlib {
     	}
     
     	template<typename Iterable, typename Printable>
-    	void print_iter(Iterable arg, Printable spliter)
+    	static void print_iter(Iterable arg, Printable spliter)
     	{
             for(const auto & i : arg)
     		    ::std::cout << i << spliter;
     		return;
     	}
     	template<typename Iterable, typename Printable>
-    	void println_iter(Iterable arg, Printable spliter)
+    	static void println_iter(Iterable arg, Printable spliter)
     	{
     		print_iter(arg, spliter);
     		::std::cout << ::std::endl;
     		return;
     	}
         template<typename Iterable>
-    	void print_iter(Iterable arg)
+    	static void print_iter(Iterable arg)
     	{
             for(const auto & i : arg)
     		    ::std::cout << i << ' ';
     		return;
     	}
     	template<typename Iterable>
-    	void println_iter(Iterable arg)
+    	static void println_iter(Iterable arg)
     	{
     		print_iter(arg);
     		::std::cout << ::std::endl;
     		return;
     	}
     
-        std::string scanln()
+        static std::string scanln()
         {
             ::std::string str;
             ::std::getline(::std::cin, str);
