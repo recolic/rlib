607b5d1e4db4dd53efb5dddaa5ed9dca899293d1
Recolic Keghart <root@recolic.net>
2018-04-01 16:02:35 +0800

    functional is working

--- functional.hpp
+++ functional.hpp
@@ -1,7 +1,7 @@
 #ifndef RLIB_FUNCTIONAL_HPP_
 #define RLIB_FUNCTIONAL_HPP_
 
-#include <rlib/require/cxx14>
+#include <rlib/require/cxx17>
 #include <rlib/class_decorator.hpp>
 
 #include <type_traits>
@@ -20,13 +20,23 @@ namespace rlib {
                 return f(std::forward<Args>(args) ...);
             }
         };
+        template <typename Func, typename... Args>
+        struct repeated_func_return_list {
+            using return_type = typename std::invoke_result<Func, Args ...>::type;
+            auto operator ()(size_t count, Func f, Args ... args) {
+                std::list<return_type> ret;
+                for(size_t cter = 0; cter < count; ++cter)
+                    ret.push_back(std::move(f(std::forward<Args>(args) ...)));
+                return std::move(ret);
+            }
+        };
+
     }
 }
 
 namespace rlib {
-
     template <class Func, typename... Args>
-    static inline double timed_func(::std::function<Func> f, Args && ... args)
+    static inline double time_of(Func && f, Args && ... args)
     {
         auto begin = std::chrono::high_resolution_clock::now();
         f(std::forward<Args>(args) ...);
@@ -35,23 +45,21 @@ namespace rlib {
     }
 
     template <class Func, typename... Args>
-    static inline auto repeat(size_t count, Func && f, Args && ... args)
+    constexpr static inline auto repeat(size_t count, Func && f, Args && ... args)
     {
-        //using return_type = typename std::invoke_result<Func, Args ...>::type;
-        //using return_type2 = typename std::invoke_result<typename impl::repeated_func<Func, Args ...>, size_t, Func, Args ...>::type;
-        //using return_type3 = decltype(impl::repeated_func<Func, Args ...>()(count, f, args ...));
-        //static_assert(std::is_same<return_type, return_type2>::value);
-        //static_assert(std::is_same<return_type, return_type3>::value);
+        // Unnecessary asserts for debugging.
+        using return_type = typename std::invoke_result<Func, Args ...>::type;
+        using return_type2 = typename std::invoke_result<typename impl::repeated_func<Func, Args ...>, size_t, Func, Args ...>::type;
+        using return_type3 = decltype(impl::repeated_func<Func, Args ...>()(count, f, args ...));
+        static_assert(std::is_same<return_type, return_type2>::value);
+        static_assert(std::is_same<return_type, return_type3>::value);
 
         return std::bind(impl::repeated_func<Func, Args ...>(), count, std::forward<Func>(f), std::forward<Args>(args) ...);
     }
     template <class Func, typename... Args>
-    static inline ::std::list<typename ::std::result_of<Func(Args ...)>::type> repeat_and_return_list(size_t count, Func f, Args... args)
+    constexpr static inline auto repeat_and_return_list(size_t count, Func f, Args... args)
     {
-        ::std::list<typename ::std::result_of<Func(Args ...)>::type> ret;
-        for(size_t cter = 0; cter < count; ++cter)
-            ret.push_back(std::move(f(args ...)));
-        return std::move(ret);
+        return std::bind(impl::repeated_func_return_list<Func, Args ...>(), count, std::forward<Func>(f), std::forward<Args>(args) ...);
     }
 }
 #endif
--- test/src/print_performance.cc
+++ test/src/print_performance.cc
@@ -1,7 +1,19 @@
 #include <rlib/functional.hpp>
 #include <rlib/stdio.hpp>
+using namespace rlib;
+
+#define test_str "This is some test string."
+#define test_times 1000000
 
 int main() {
-    rlib::timed_func();
-
+    println(time_of(repeat(test_times, []{
+                    println(test_str);
+                    })));
+    println(time_of(repeat(test_times, []{
+                    std::cout << test_str << std::endl;
+                    })));
+    println(time_of(repeat(test_times, []{
+                    std::printf(test_str);
+                    std::printf("\n");
+                    })));
 }
