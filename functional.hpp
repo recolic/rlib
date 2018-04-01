b696af05421c93a7012898f75639285e7885356d
Recolic Keghart <root@recolic.net>
2018-04-01 15:07:55 +0800

    DO NOT PULL: functional under working

--- functional.hpp
+++ functional.hpp
@@ -12,33 +12,40 @@
 namespace rlib {
     namespace impl {
         template <typename Func, typename... Args>
-        struct repeated_func {
-            using return_type = typename std::result_of<Func(Args ...)>::type;
-            return_type operator ()(size_t count, Func &&f, Args && ... args) {
-                for(size_t cter = 0; cter < count - 1; ++cter)
-                    f(args ...);
-                return f(args ...);
-            }
-        };
+        auto repeated_func(size_t count, const Func &f, Args && ... args) {
+            for(size_t cter = 0; cter < count - 1; ++cter)
+                f(std::forward<Args>(args) ...);
+            return f(std::forward<Args>(args) ...);
+        }
+//         struct repeated_func {
+//            using return_type = typename std::invoke_result<Func, Args ...>::type;
+//            auto operator ()(size_t count, const Func &f, Args && ... args) {
+//                for(size_t cter = 0; cter < count - 1; ++cter)
+//                    f(std::forward<Args>(args) ...);
+//                return f(std::forward<Args>(args) ...);
+//            }
+//        };
     }
 }
 
 namespace rlib {
 
     template <class Func, typename... Args>
-    static inline double timed_func(::std::function<Func> f, Args... args)
+    static inline double timed_func(::std::function<Func> f, Args && ... args)
     {
         auto begin = std::chrono::high_resolution_clock::now();
-        f(args ...);
+        f(std::forward<Args>(args) ...);
         auto end = std::chrono::high_resolution_clock::now();
         return ::std::chrono::duration<double>(end - begin).count(); 
     }
 
     template <class Func, typename... Args>
-    static inline auto repeat(size_t count, Func &&f, Args && ... args)
+    static inline auto repeat(size_t count, const Func &f, Args && ... args)
     {
-        //<typename impl::repeated_func<Func, Args ...>::return_type (void)> 
-        return std::bind((impl::repeated_func<Func, Args ...>()), args ...);
+        using return_type = typename std::invoke_result<Func, Args ...>::type;
+        std::function<return_type(size_t, const Func &, Args ...)> wrapped_func (impl::repeated_func<Func, Args ...>());
+        auto bind_result = std::bind(wrapped_func, count, f, std::forward<Args>(args) ...);
+        return std::function<return_type(void)>(bind_result);
     }
     template <class Func, typename... Args>
     static inline ::std::list<typename ::std::result_of<Func(Args ...)>::type> repeat_and_return_list(size_t count, Func f, Args... args)
--- test/src/repeat.cc
+++ test/src/repeat.cc
@@ -3,9 +3,20 @@
 
 
 int main() {
-    auto f = rlib::repeat(4, [](int i){
-            rlib::println("i is", i);
-            }, 777);
+//    auto f = rlib::repeat(4, [](int i){
+//            rlib::println("i is", i);
+//            }, 777);
+//    f();
+    auto m = [](int i){
+        rlib::println("i is", i);
+    };
+    auto f = rlib::repeat(4, m, 777);
+//    f(4,m,444);
+//    auto ff = std::bind(&decltype(f)::operator(), &f, 4, m, 444);
+    //std::function<void(size_t, decltype(m), int)> goodf(f);
+
+//    auto ff = std::bind(f, 4,m,444);
     f();
+
 }
 
