23f3ee84ec25a492010a121fa804f31e7def8195
Recolic Keghart <root@recolic.net>
2018-04-07 19:17:10 +0800

    test update

--- test/src/print_performance.cc
+++ test/src/print_performance.cc
@@ -1,19 +1,21 @@
 #include <rlib/functional.hpp>
-#include <rlib/stdio.hpp>
+#include <iostream>
 using namespace rlib;
+#include <cstdio>
 
 #define test_str "This is some test string."
 #define test_times 1000000
 
 int main() {
-    println(timeof(repeat(test_times, []{
-                   println(test_str);
-                   })));
-    println(timeof(repeat(test_times, []{
-                   std::cout << test_str << std::endl;
-                   })));
-    println(timeof(repeat(test_times, []{
+//    println(timeof(repeat(test_times, []{
+//                   println(test_str);
+//                   })));
+    std::ios::sync_with_stdio(false);
+    std::cerr << (timeof(repeat(test_times, []{
+                   std::cout << test_str << "\n";
+                   }))) << std::endl;
+    std::cerr << (timeof(repeat(test_times, []{
                    std::printf(test_str);
                    std::printf("\n");
-                   })));
+                   }))) << std::endl;
 }
