fb2d57114e07894a5ba9a84383578d9b57b471c5
Recolic Keghart <root@recolic.net>
2018-04-16 15:56:05 +0800

    reconstructed rlib::string. UNFINISHED so DO NOT USE IT

--- string/string.hpp
+++ string/string.hpp
@@ -17,54 +17,181 @@
 #include <cstdarg>
 #include <cstdio>
 #include <cstdlib>
+#include <cstring>
 #include <stdexcept>
 #include <sstream>
 #include <type_traits>
 
 namespace rlib {
-//TODO: Pass args by rvalue reference.
+    // literals::_format, format_string, string::format
+    namespace impl {
+        template<typename StdString>
+        void _format_string_helper(std::stringstream &ss, const StdString &fmt) {
+            ss << fmt;
+        }
+        template<typename Arg1, typename... Args>
+        void _format_string_helper(std::stringstream &ss, const std::string &fmt, Arg1 arg1, Args... args) {
+            size_t pos = 0;
+            while((pos = fmt.find("{}")) != std::string::npos) {
+                if(pos != 0 && fmt[pos-1] == '\\') {
+                    ++pos;
+                    continue;
+                }
+                ss << fmt.substr(0, pos) << arg1;
+                _format_string_helper(ss, fmt.substr(pos + 2), args ...);
+                return;
+            }
+		_format_string_helper(ss, fmt);
+        }
+        template<typename... Args>
+        std::string format_string(const std::string &fmt, Args... args) {
+            std::stringstream ss;
+            _format_string_helper(ss, fmt, args...);
+            return ss.str();
+        }
+    }
+
+    // format_string_c, string::cformat
+    namespace impl {
+        inline char *_format_string_c_helper(const char *fmt, ...)
+        {
+            int n;
+            int size = std::strlen(fmt);     /* Guess we need no more than 100 bytes */
+            char *p, *np;
+            va_list ap;
+
+            if ((p = (char *)malloc(size)) == NULL)
+                throw std::runtime_error("malloc returns null.");
+
+            while (1) {
+                va_start(ap, fmt);
+                n = vsnprintf(p, size, fmt, ap);
+                va_end(ap);
+
+                if (n < 0)
+                    throw std::runtime_error("vsnprintf returns " + std::to_string(n));
+                if (n < size)
+                    return p;
+
+                size = n + 1;
+
+                if ((np = (char *)realloc (p, size)) == NULL) {
+                    free(p);
+                    throw std::runtime_error("make_message realloc failed.");
+                } else {
+                    p = np;
+                }
+            }
+        }
+        template<typename... Args>
+        std::string format_string_c(const std::string &fmt, Args... args)
+        {
+            char *res = _format_string_c_helper(fmt.c_str(), args ...);
+            std::string s = res;
+            free(res);
+            return std::move(s);
+        }
+    }
 
     class string : public std::string {
-        std::vector<string> split(const char &divider = ' ');
-        std::vector<string> split(const std::string &divider);
+    public:
+        using std::string::string;
+        // Warning: Allocator not supported. Some unusual constructed not supported.
+        string(const std::string &s) : std::string(s) {}
+        string(std::string &&s) : std::string(std::forward<std::string>(s)) {}
+        //string(const char *s, size_t count) : std::string(s, count) {}
+        //string(const char *s) : std::string(s) {}
+        //string(size_t count, char c) : std::string(count, c) {}
+        //string(const std::string &other, size_t pos) : std::string(other, pos) {}
+        //string(const std::string &other, size_t pos, size_t count) : std::string(other, pos, count) {}
+        //template<typename InputIt> string(InputIt first, InputIt last) : std::string(first, last) {}        
+
+        std::vector<string> split(const char &divider = ' ') const {
+            const string &toSplit = *this;
+            std::vector<string> buf;
+            size_t curr = 0, prev = 0;
+            while((curr = toSplit.find(divider, curr)) != std::string::npos) {
+                buf.push_back(toSplit.substr(prev, curr - prev));
+                ++curr; // skip divider
+                prev = curr;
+            }
+            buf.push_back(toSplit.substr(prev));
+            return std::move(buf);
+        }
+        std::vector<string> split(const std::string &divider) const {
+            const string &toSplit = *this;
+            std::vector<string> buf;
+            size_t curr = 0, prev = 0;
+            while((curr = toSplit.find(divider, curr)) != std::string::npos) {
+                buf.push_back(toSplit.substr(prev, curr - prev));
+                curr += divider.size(); // skip divider
+                prev = curr;
+            }
+            buf.push_back(toSplit.substr(prev));
+            return std::move(buf);
+        }
 
         template <class ForwardIterator>
-        string join(ForwardIterator begin, ForwardIterator end);
+        string join(ForwardIterator begin, ForwardIterator end) const {
+            const string &toJoin = *this;
+            std::string result;
+            for(ForwardIterator iter = begin; iter != end; ++iter) {
+                if(iter != begin)
+                    result += toJoin;
+                result += *iter;
+            }
+            return std::move(result);
+        }
         template <class ForwardIterable>
-        string join(const ForwardIterable &buffer);
+        string join(const ForwardIterable &buffer) const {
+            return std::move(this->join(buffer.cbegin(), buffer.cend()));
+        }
 
         template <typename ForwardIterator>
-        string strip(ForwardIterator begin, ForwardIterator end);
+        string strip(ForwardIterator begin, ForwardIterator end) const;
         template <class ForwardIterable>
-        string strip(const ForwardIterable &strippedCharBuffer);
-        template <>
-        string strip(char stripped = ' ');
+        string strip(const ForwardIterable &strippedCharBuffer) const;
+        string strip(char stripped = ' ') const {
+            const string &toStrip = *this;
+            auto len = toStrip.size();
+            size_t begin = 0;
+            size_t end = len - 1;
+            while(toStrip[begin] == stripped) ++begin;
+            while(end != begin && toStrip[end] == stripped) --end;
+            return std::move(toStrip.substr(begin, end+1));
+        }
 
-        size_t replace(const std::string &from, const std::string &to);
-        bool replace_once(const std::string &from, const std::string &to);
+        //TODO: Change name so it won't fight with std::string
+        size_t replace_inplace(const std::string &from, const std::string &to) {
+            if(from.empty())
+                return 0;
+            size_t start_pos = 0;
+            size_t times = 0;
+            while((start_pos = find(from, start_pos)) != std::string::npos)
+            {
+                ++times;
+                replace(start_pos, from.length(), to);
+                start_pos += to.length(); // In case 'to' contains 'from', like replacing 'x' with 'yx'
+            }
+            return times;
+        }
+        bool replace_once_inplace(const std::string &from, const std::string &to) {
+            size_t start_pos = find(from);
+            if(start_pos == std::string::npos)
+                return false;
+            replace(start_pos, from.length(), to);
+            return true;
+        }
 
         template <typename... Args>
-        string format(Args... args);
+        string format(Args... args) const {
+            return std::move(impl::format_string(*this, args ...));
+        }
+        template <typename... Args>
+        string cformat(Args... args) const {
+            return std::move(impl::format_string_c(*this, args ...));
+        }
     };
-	std::vector<std::string> splitString(const std::string &toSplit, const char &divider = ' ');
-	std::vector<std::string> splitString(const std::string &toSplit, const std::string &divider);
-    template <class ForwardIterator>
-    std::string joinString(const char &toJoin, ForwardIterator begin, ForwardIterator end);
-    template <class ForwardIterator>
-    std::string joinString(const std::string &toJoin, ForwardIterator begin, ForwardIterator end);
-    template <class ForwardIterable>
-    std::string joinString(const char &toJoin, ForwardIterable begin, ForwardIterable end);
-    template <class ForwardIterable>
-    std::string joinString(const std::string &toJoin, ForwardIterable begin, ForwardIterable end);
-
-    std::string stripString(const std::string &toStrip, char stripped = ' ');
-  
-    size_t replaceSubString(std::string& str, const std::string &from, const std::string& to);
-    bool replaceSubStringOnce(std::string& str, const std::string& from, const std::string& to);
-    template <typename... Args>
-    std::string format_string_c(const std::string &fmt, Args... args);
-    template <typename... Args>
-    std::string format_string(const std::string &fmt, Args... args);
 
     namespace impl {
         struct formatter {
@@ -72,7 +199,7 @@ namespace rlib {
             formatter(std::string &&fmt) : fmt(fmt) {}
             template <typename... Args>
             std::string operator ()(Args... args) {
-                return rlib::format_string(fmt, args ...);
+                return std::move(rlib::impl::format_string(fmt, args ...));
             }
 
             std::string fmt;
@@ -85,172 +212,6 @@ namespace rlib {
         }
     }
 
-//Implements.
-    char *_format_string_c_helper(const char *fmt, ...);
-    template<typename... Args>
-    std::string format_string_c(const std::string &fmt, Args... args)
-    {
-        char *res = _format_string_c_helper(fmt.c_str(), args ...);
-        std::string s = res;
-        free(res);
-        return std::move(s);
-    }
-
-    template<typename StdString>
-    void _format_string_helper(std::stringstream &ss, const StdString &fmt) {
-		static_assert(std::is_same<StdString, std::string>::value, "incorrect argument type to _format_string_helper");
-        ss << fmt;
-    }
-    template<typename Arg1, typename... Args>
-    void _format_string_helper(std::stringstream &ss, const std::string &fmt, Arg1 arg1, Args... args) {
-        size_t pos = 0;
-        while((pos = fmt.find("{}")) != std::string::npos) {
-            if(pos != 0 && fmt[pos-1] == '\\') {
-                ++pos;
-                continue;
-            }
-            ss << fmt.substr(0, pos) << arg1;
-            _format_string_helper(ss, fmt.substr(pos + 2), args ...);
-            return;
-        }
-		_format_string_helper(ss, fmt);
-    }
-    template<typename... Args>
-    std::string format_string(const std::string &fmt, Args... args) {
-        std::stringstream ss;
-        _format_string_helper(ss, fmt, args...);
-        return ss.str();
-    }
-
-	inline std::vector<std::string> splitString(const std::string &toSplit, const char &divider)
-	{
-        std::vector<std::string> buf;
-        size_t curr = 0, prev = 0;
-        while((curr = toSplit.find(divider, curr)) != std::string::npos) {
-            buf.push_back(toSplit.substr(prev, curr - prev));
-            ++curr; // skip divider
-            prev = curr;
-        }
-        buf.push_back(toSplit.substr(prev));
-        return std::move(buf);
-	}
-    inline std::vector<std::string> splitString(const std::string &toSplit, const std::string &divider)
-	{
-        std::vector<std::string> buf;
-        size_t curr = 0, prev = 0;
-        while((curr = toSplit.find(divider, curr)) != std::string::npos) {
-            buf.push_back(toSplit.substr(prev, curr - prev));
-            curr += divider.size(); // skip divider
-            prev = curr;
-        }
-        buf.push_back(toSplit.substr(prev));
-        return std::move(buf);
-	}
-    template <class ForwardIterator>
-    std::string joinString(const char &toJoin, ForwardIterator begin, ForwardIterator end) {
-        std::string result;
-        for(ForwardIterator iter = begin; iter != end; ++iter) {
-            if(iter != begin)
-                result += toJoin;
-            result += *iter;
-        }
-        return std::move(result);
-    }
-    template <class ForwardIterator>
-    std::string joinString(const std::string &toJoin, ForwardIterator begin, ForwardIterator end) {
-        std::string result;
-        for(ForwardIterator iter = begin; iter != end; ++iter) {
-            if(iter != begin)
-                result += toJoin;
-            result += *iter;
-        }
-        return std::move(result);
-    }
-    template <class ForwardIterable>
-    std::string joinString(const std::string &toJoin, ForwardIterable buf) {
-        auto begin = buf.begin();
-        auto end = buf.end();
-        return std::move(joinString(toJoin, begin, end));
-    }
-    template <class ForwardIterable>
-    std::string joinString(const char &toJoin, ForwardIterable buf) {
-        auto begin = buf.begin();
-        auto end = buf.end();
-        return std::move(joinString(toJoin, begin, end));
-    }
-
-    inline std::string stripString(const std::string &toStrip, char stripped) {
-        auto len = toStrip.size();
-        size_t begin = 0;
-        size_t end = len - 1;
-        while(toStrip[begin] == stripped) ++begin;
-        while(end != begin && toStrip[end] == stripped) --end;
-        return std::move(toStrip.substr(begin, end+1));
-    }
-
-    inline size_t replaceSubString(std::string& str, const std::string &from, const std::string& to) 
-    {
-        if(from.empty())
-            return 0;
-        size_t start_pos = 0;
-        size_t times = 0;
-        while((start_pos = str.find(from, start_pos)) != std::string::npos)
-        {
-            ++times;
-            str.replace(start_pos, from.length(), to);
-            start_pos += to.length(); // In case 'to' contains 'from', like replacing 'x' with 'yx'
-        }
-        return times;
-    }
-    inline bool replaceSubStringOnce(std::string& str, const std::string& from, const std::string& to) 
-    {
-        size_t start_pos = str.find(from);
-        if(start_pos == std::string::npos)
-            return false;
-        str.replace(start_pos, from.length(), to);
-        return true;
-    }
-
-    inline char *_format_string_c_helper(const char *fmt, ...)
-    {
-        int n;
-        int size = 100;     /* Guess we need no more than 100 bytes */
-        char *p, *np;
-        va_list ap;
-
-        if ((p = (char *)malloc(size)) == NULL)
-            throw std::runtime_error("malloc returns null.");
-
-        while (1) {
-
-            /* Try to print in the allocated space */
-
-            va_start(ap, fmt);
-            n = vsnprintf(p, size, fmt, ap);
-            va_end(ap);
-
-            /* Check error code */
-
-            if (n < 0)
-                throw std::runtime_error("vsnprintf returns " + std::to_string(n));
-
-            /* If that worked, return the string */
-
-            if (n < size)
-                return p;
-
-            /* Else try again with more space */
-
-            size = n + 1;       /* Precisely what is needed */
-
-            if ((np = (char *)realloc (p, size)) == NULL) {
-                free(p);
-                throw std::runtime_error("make_message realloc failed.");
-            } else {
-                p = np;
-            }
-        }
-    }
 
 }
 
--- test/src/str.cc
+++ test/src/str.cc
@@ -2,14 +2,12 @@
 using namespace rlib;
 
 #include <rlib/stdio.hpp>
-#include <string>
-using namespace std;
 
 void test(const string &s)
 {
-    println_iter(splitString(s));
-    println_iter(splitString(s, " w"s));
-    println(">>>", joinString("|", splitString(s)));
+    println_iter(s.split());
+    println_iter(s.split("w"));
+    println(">>>", string("|").join(s.split()));
 }
 
 int main()
@@ -19,11 +17,11 @@ int main()
     test("");
     test(">PAQ P<DSP<")   ;
 
-    println(stripString(" 87sd6 8s7d5 8    8 75    ") + "|");
+    println(string(" 87sd6 8s7d5 8    8 75    ").strip() + "|");
     string s = "fuck             you                  r mom 34qwo0 ghwerf 0832 ";
-    replaceSubString(s, "ghw", "fuck you");
+    s.replace_inplace("ghw", "fuck you");
     println(s);
-    replaceSubString(s, "   ", "  ");
+    s.replace_inplace("    ", "  ");
     println(s);
     return 0;
 }
