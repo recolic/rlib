41c6e2fdc681e6065f565bf1728e786aec85b551
Recolic Keghart <root@recolic.net>
2018-08-08 22:38:38 +0800

    add a naive initBandledMatrix

--- test/src/wwEigen.cc
+++ test/src/wwEigen.cc
@@ -7,5 +7,15 @@ int main() {
     Matrix<int, 16, 12> toInit;
     initBandMatrix<2, 3>(toInit, {1,2,3,4});
     std::cout << toInit <<std::endl<< getBandMatrix<int, 8, 14, 3, 1>({5,6,7}) << std::endl;
+
+    Matrix<float, 10, 10> myEmptyFloatMatrix = Matrix<float, 10, 10>::Zero();
+    initBandledMatrix(myEmptyFloatMatrix, std::map<int, float>{
+                                                                {1, 1.0},
+                                                                {4, 4.0},
+                                                                {-1, 2.0},
+                                                                {-5, 3.0},
+                                                                {-6, 5.0}
+                                                              }, 7, 6);
+    std::cout << myEmptyFloatMatrix << std::endl;
     return 0;
 }
