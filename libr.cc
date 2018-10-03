14825e82a98c8efabb3324b20011f33f01c89066
Recolic Keghart <root@recolic.net>
2018-10-03 13:30:12 +0800

    stream.hpp fix for cxx14

--- libr.cc
+++ libr.cc
@@ -1,4 +1,5 @@
 #include <rlib/log.hpp> //log_level_t
+#include <rlib/stream.hpp>
 #include <sstream>
 namespace rlib {
     namespace impl {
@@ -6,5 +7,12 @@ namespace rlib {
         int max_predefined_log_level = (int)log_level_t::DEBUG;
         thread_local std::stringstream _format_string_helper_ss;
         thread_local std::stringstream to_string_by_sstream_ss;        
+
+#if RLIB_CXX_STD < 2017
+        NullStreamBuf null_streambuf;
+#endif
     }
+#if RLIB_CXX_STD < 2017
+    std::ostream null_stream(&impl::null_streambuf);
+#endif
 }
--- stream.hpp
+++ stream.hpp
@@ -1,13 +1,14 @@
+#include <rlib/require/cxx11>
 #include <streambuf>
 #include <iostream>
 #include <rlib/sys/os.hpp>
 
+// I'm testing this macro. if it works good, I'll apply it to rlib.
 #if RLIB_CXX_STD >= 2017
 #define RLIB_IMPL_VAR_DECL inline
 #else
 #define RLIB_IMPL_VAR_DECL extern
 #endif
-
 
 namespace rlib {
     namespace impl {
